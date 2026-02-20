bc7fec29c3baa5e19223e6cb8a2e1b85bac045da
Deissy Duran <d.vivianaduran.r@gmail.com>
2026-02-20 01:24:43 -0500

    fix: resolve broken image paths in production

---
+++ src/assets/images/planImages.ts
@@ -0,0 +1,13 @@
+import recintoDelPensamiento from "./recinto-del-pensamiento.jpg";
+import atardeceresDeCafe from "./atardeceres-de-cafe.jpg";
+import haciendaLindaraja from "./hacienda-lindaraja.jpg";
+import renovacionVotos from "./renovacion-votos.jpg";
+import tourPaisajeCultural from "./tour-paisaje-cultural.jpg";
+
+export const planImages = {
+  recintoDelPensamiento,
+  atardeceresDeCafe,
+  haciendaLindaraja,
+  renovacionVotos,
+  tourPaisajeCultural,
+};
